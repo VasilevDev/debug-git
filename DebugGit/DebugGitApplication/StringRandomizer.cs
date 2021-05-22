0b150f478391d3d7c76b76184fb67526d5b432f0
Vasilev Alexey <37271382+VasilevDev@users.noreply.github.com>
2021-05-22 23:36:52 +0600

    Add StringRandomizer

---
+++ DebugGit/DebugGitApplication/StringRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace DebugGitApplication
+{
+	public static class StringRandomizer
+	{
+		private static Random random;
+
+		static StringRandomizer()
+		{
+			random = new Random();
+		}
+
+		public static string GetString(int length = 10)
+		{
+			return new string(Enumerable
+				.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length)
+				.Select(s => s[random.Next(s.Length)]).ToArray()
+			);
+		}
+	}
+}
