b455bd4d5ac8b7f0011e84844a40de05bc0ad9fc
Vasilev Alexey <37271382+VasilevDev@users.noreply.github.com>
2021-05-24 17:47:51 +0600

    Add Main comment

--- DebugGit/DebugGitApplication/Program.cs
+++ DebugGit/DebugGitApplication/Program.cs
@@ -4,6 +4,10 @@ namespace DebugGitApplication
 {
 	class Program
 	{
+		/// <summary>
+		/// Main
+		/// </summary>
+		/// <param name="args"></param>
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Application started...");
